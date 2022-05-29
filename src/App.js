70470d4efc4dc0fe13384d301c00dd1ac295ed2c
aedeso <adsbet.aa@gmail.com>
2022-05-29 20:00:40 +0200

    RatingStar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Datamuse } from "./components/datamuse/Datamuse";
+// import { Datamuse } from "./components/datamuse/Datamuse";
 // import { Api } from "./components/smashingmagazine/api";
 // import { Calculator } from "./components/calculator/Calculator";
+import { RatingStar } from "./components/rating/RatingStar";
 
-
-const App = () => <Datamuse />
+const App = () => <RatingStar />
 
 export default App
---
+++ src/components/rating/RatingStar.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import {FaStar} from "react-icons/fa"
+
+const Star = ({selected = false, onClick}) => {
+    return (
+        <FaStar color={selected ? "yellow" : "black"} onClick={onClick} />
+    )
+}
+
+export const RatingStar = ({totalStars=10}) => {
+    const [selection, setSelection] = useState(0);
+    return (
+        <>
+            {
+            [...Array(totalStars)].map((e,i) => 
+            <Star 
+            key={i} 
+            selected={selection > i}
+            onClick={()=> setSelection(i+1)}
+            />)
+        }
+        </>
+        
+    )
+}
